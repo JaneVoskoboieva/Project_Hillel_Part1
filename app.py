40e7564256e08ed77ac64ee07d56e11e5a11b9d2
Jane Voskoboieva <kareglazaya1000@gmail.com>
2021-07-22 22:14:03 +0300

    Finished work with Flask. Issue with database connect

---
+++ app.py
@@ -0,0 +1,53 @@
+# pip install -r requirements.txt
+from flask import Flask, request, render_template, flash
+import sql_module
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def start_page_of_blog():
+    """ Function returns all records hat we have in our database"""
+    all_records = sql_module.all_records()
+    return render_template("index.html", title="All Records in the Blog are:", all_records=all_records)
+
+@app.route('/new_record', methods=['POST', 'GET'])
+def create_record():
+    """Function for adding new record"""
+    if request.method == "POST":
+        title = request.form['new_record_title']
+        description = request.form['new_record_description']
+        sql_module.add_record(title, description)
+        flash(f"New record was added to the blog")
+    return render_template("new_record.html")
+
+@app.route('/edit', methods=['POST', 'GET'])
+def edit_records():
+    if request.method == "POST":
+        try:
+            identifier = int(request.form["id_for_edit"])
+        except ValueError:
+            flash(f"Please use numbers for the ID value")
+
+        title = request.form['editing_title']
+        description = request.form['editing_description']
+        sql_module.edit_record(identifier, title, description)
+        flash(f"The record was updated")
+
+    return render_template("edit_record.html")
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete_record():
+    if request.method == "POST":
+        try:
+            identifier = int(request.form["record_id_for_deleting"])
+        except ValueError:
+            flash(f"Please use numbers for the ID value")
+
+        sql_module.delete_record(identifier)
+        flash(f"Record was deleted")
+
+    return render_template("delete_record.html", identifier=id)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ sql_module.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+def create_database():
+    """Function for creating database"""
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    sql = """CREATE TABLE IF NOT EXISTS records (
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Title TEXT
+    Description TEXT)"""
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    return cursor
+
+def all_records():
+    """Function for showing up all records from the blog"""
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    cursor.execute("""SELECT * FROM records""")
+    all_records = cursor.fetchall()
+    return all_records
+
+def add_record(title, description):
+    """Function for adding records to the blog
+    required:
+    title - text
+    description - text"""
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    sql = f'INSERT INTO records (Title, Description) VALUES ("{title}", "{description}")'
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    return None
+
+def edit_record(identifier, title, description):
+    """Function edit records
+    required:
+    identifier - integer
+    title - text
+    description - text"""
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    sql = f"UPDATE records SET Title = {title}, Description = {description} WHERE id = {identifier}"
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    return None
+
+def delete_record(identifier):
+    """Function deletes record
+    required:
+    identifier - integer"""
+
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    sql = "DELETE FROM records WHERE id = {identifier}"
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    return None
